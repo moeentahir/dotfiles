a2df907fd9751654d2020ee49b8289d662bcd7ae
Moeen Tahir <mtahir@thoguthworks.com>
2020-12-31 19:10:47 +0000

    Homebrew setup for linux
    Also combined all the homebrew packages in one task

---
+++ setup-homebrew.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
